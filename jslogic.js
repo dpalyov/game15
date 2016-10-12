2e3424a2c16aed193936f9b7018887cf2a60d732
panayotovn <nikolapanayotov@gmail.com>
2016-10-12 22:35:19 +0300

    Draw and TryMove functions

--- jslogic.js
+++ jslogic.js
@@ -24,3 +24,43 @@ function drawGrid() {
         }
     }
 }
+
+function randomizeGrid() {
+    let i = 0;
+
+    while (i < (300*gameSize)) {
+        let rndEl = parseInt((Math.random() * ((gameSize*gameSize)-1)) + 1);
+        let ElName = "div_" + rndEl;
+
+        if (tryMove(ElName,$("[name='" + ElName + "']").attr("id"),0) == true) {
+            i++;
+        }
+    }
+
+    //start the timer and clear the counters
+    //refreshScores();
+
+    //let startDate = new Date();
+    //var numClicks = 0;
+}
+
+function tryMove(name, loc, speed) {
+    let elX = parseInt(loc.split("_")[1]);
+    let elY = parseInt(loc.split("_")[0]);
+
+    if ( ( (elX == hole.x) && (elY == hole.y+1 || elY == hole.y-1) ) || ( (elY == hole.y) && (elX == hole.x+1 || elX == hole.x-1) ) ) {
+        $("[name='" + name + "']").animate({left: (hole.x-1) * 152 + "px", top: (hole.y-1) * 152 + "px"}, speed);
+        $("[name='" + name + "']").attr("id",hole.y + "_" + hole.x);
+
+        hole.x = elX;
+        hole.y = elY;
+
+        if (speed != 0 ) {
+            numClicks += 1;
+        }
+
+        return true;
+    } else {
+        return false;
+    }
+}
